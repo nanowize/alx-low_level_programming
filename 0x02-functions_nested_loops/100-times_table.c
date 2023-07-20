7ef3926394b9163d8f7d29ccf994e315aa34c123
nanowize <talk2encas4sci@gmail.com>
2023-07-20 00:08:08 -0700

    Resolving

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,10 +3,14 @@
 	 * print_times_table - prints the n times table, starting with 0
 	 * @n: number of the times table
 	 */
-void print_times_table(int n)	
+
+void print_times_table(int n)
+
 {
+
 	int i, j, k;
 	if (n >= 0 && n <= 15)
+
 	{
 		for (i = 0; i <= n; i++)
 		{
