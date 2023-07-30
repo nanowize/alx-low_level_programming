67d7e4c4f9fb61dd1332af73a9fac2901062f604
nanowize <talk2encas4sci@gmail.com>
2023-07-29 17:20:45 -0700

    Project 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper programme checks for uppercase character
+ * c: Variable text
+ * Return: Always 0.
+ * Project: Done by Chuks nnadozie
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+0x04. C - More functions, more nested loops
+About Ten Projects to be completed by me Chuks Nnadozie
