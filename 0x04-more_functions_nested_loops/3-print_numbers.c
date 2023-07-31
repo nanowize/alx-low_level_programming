2dd3edd28606c9399ced5bee0c64c4759e370332
nanowize <talk2encas4sci@gmail.com>
2023-07-30 23:45:31 -0700

    3-print_numbers.c push

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0-9
+ * return: void
+ * Project done by Chuks Nnadozie
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for ( = 40';c <= '50'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
