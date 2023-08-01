bc30a2d1560cbae08fad10ff1a8387192ed8163e
nanowize <talk2encas4sci@gmail.com>
2023-08-01 08:16:25 -0700

    6-print_line.c push

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,7 +3,6 @@
 /**
  * print_line - draws a straight line in the terminal
  * @n: number of times the character _ should be printed
- * Project done by Chuks Nnadozie
  */
 void print_line(int n)
 {
